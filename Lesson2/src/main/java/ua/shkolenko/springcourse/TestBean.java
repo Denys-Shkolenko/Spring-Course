26ed22afbc72b9719bd5b1fbb4dd69b00c73ee09
Denys-Shkolenko <denys.shkolenko@gmail.com>
2022-07-28 15:18:00 +0300

    finish

--- Lesson2/src/main/java/ua/shkolenko/springcourse/TestBean.java
+++ Lesson2/src/main/java/ua/shkolenko/springcourse/TestBean.java
@@ -1,2 +1,17 @@
-package ua.shkolenko.springcourse;public class TestBean {
+package ua.shkolenko.springcourse;
+
+public class TestBean {
+    private String name;
+
+    public TestBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- Lesson2/src/main/java/ua/shkolenko/springcourse/TestSpring.java
+++ Lesson2/src/main/java/ua/shkolenko/springcourse/TestSpring.java
@@ -1,2 +1,17 @@
-package ua.shkolenko.springcourse;public class TestSpring {
+package ua.shkolenko.springcourse;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "ApplicationContext.xml"
+        );
+
+        TestBean testBean = context.getBean("testBean", TestBean.class);
+
+        System.out.println(testBean.getName());
+
+        context.close();
+    }
 }
